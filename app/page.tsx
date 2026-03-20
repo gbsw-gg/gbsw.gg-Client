4a0eaef3a5fc59ea7e0755bdcedc41f8c6106bb8
yihyle <hslee050320@naver.com>
2026-03-20 10:02:21 +0900

    fix: 폴더명 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from "react";
-import { Icons } from "@/Icons";
+import { Icons } from "@/icons";
 import Image from "next/image";
 
 export default function Home() {
